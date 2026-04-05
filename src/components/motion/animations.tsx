1a8dc502c69f61409ca32116e926574672dbb254
znythlabs <znythlabs@gmail.com>
2026-04-05 20:10:28 +0800

    chore: remove parallax motion effect on all portfolio images

--- src/components/motion/animations.tsx
+++ src/components/motion/animations.tsx
@@ -43,19 +43,13 @@ export const ParallaxImage = ({
     scale = 1.25,
 }: ParallaxImageProps) => {
     const ref = useRef<HTMLDivElement>(null);
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ['start end', 'end start'],
-    });
-    const y = useTransform(scrollYProgress, [0, 1], [-100 * speed, 100 * speed]);
 
     return (
         <div
             ref={ref}
             className={`overflow-hidden relative flex items-center justify-center ${className}`}
         >
-            <motion.img
-                style={{ y, scale }}
+            <img
                 src={src}
                 alt={alt}
                 className={`w-full h-full object-cover origin-center ${imgClassName}`}
